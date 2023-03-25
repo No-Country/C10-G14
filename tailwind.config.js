8c7e84911dd59399ad7843f72d3a279717df7f2b
jcda23 <jcda_23@hotmail.com>
2023-03-25 01:16:41 -0500

    Creación de proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "orange-custom": "#bd4413",
+        "gray-input": "#808080",
+        "body-color": "#FFFFFF",
+        "body-card-color": "##E6E6E6",
+        "header-color": "#0C090D",
+        "first-blue": "#52EFC2",
+        "first-black": "#0C090D",
+        "first-yellow": "#F9C22E",
+        "error-color": "#E01A4F",
+        "placeholde-color": "#808080",
+      },
+    },
+  },
+  plugins: [],
+};
