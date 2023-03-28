78a56653d352f6efd5020ffacc35ae35a9501bff
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-03-28 20:53:27 -0300

    SP1-WF44: Corregido typo en PaymentRepository

--- Backend/WorldFitBackend/src/main/java/com/C10G14/WorldFitBackend/repository/PaymentRepository.java
+++ Backend/WorldFitBackend/src/main/java/com/C10G14/WorldFitBackend/repository/PaymentRepository.java
@@ -1,6 +1,7 @@
 package com.C10G14.WorldFitBackend.repository;
 
+import com.C10G14.WorldFitBackend.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentRepository extends JpaRepository<PaymentRepository, Long> {
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
