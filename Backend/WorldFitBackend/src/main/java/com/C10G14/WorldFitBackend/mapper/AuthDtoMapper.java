a9d72cf66807e1cd7cb0ff61cea2cd3bb2104167
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-04-04 14:50:22 -0300

    SP2-WF70: Agregados atributos a entidad, servicio y dto usuario, y creado authMapper.

---
+++ Backend/WorldFitBackend/src/main/java/com/C10G14/WorldFitBackend/mapper/AuthDtoMapper.java
@@ -0,0 +1,38 @@
+package com.C10G14.WorldFitBackend.mapper;
+
+import com.C10G14.WorldFitBackend.dto.RegisterRequestDto;
+import com.C10G14.WorldFitBackend.entity.Role;
+import com.C10G14.WorldFitBackend.entity.User;
+import com.C10G14.WorldFitBackend.enumeration.ERole;
+import com.C10G14.WorldFitBackend.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class AuthDtoMapper {
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public User requestToEntity(RegisterRequestDto registerRequestDto){
+        List<Role> roles = new ArrayList<Role>();
+        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                .orElseThrow(()-> new RuntimeException("Role USER not found"));
+        roles.add(userRole);
+        return User.builder()
+                .email(registerRequestDto.getUsername())
+                .password(passwordEncoder.encode(registerRequestDto.getPassword()))
+                .profileImg(registerRequestDto.getProfilePicture())
+                .sex(registerRequestDto.getSex())
+                .age(registerRequestDto.getAge())
+                .height(registerRequestDto.getHeight())
+                .weight(registerRequestDto.getWeight())
+                .role(roles)
+                .build();
+    }
+}
