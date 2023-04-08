19c9e13a945992965ca3edf555e5a0eb84e3faeb
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-04-08 18:25:33 -0300

    SP3-WF102: Agregado Enum para tipo Sex y cambiados tipos en User entity, service y mapper

--- Backend/WorldFitBackend/src/main/java/com/C10G14/WorldFitBackend/mapper/AuthDtoMapper.java
+++ Backend/WorldFitBackend/src/main/java/com/C10G14/WorldFitBackend/mapper/AuthDtoMapper.java
@@ -1,21 +1,18 @@
 package com.C10G14.WorldFitBackend.mapper;
 
-import com.C10G14.WorldFitBackend.exception.AlreadyExistException;
-import com.C10G14.WorldFitBackend.exception.CantBeEmptyException;
 import com.C10G14.WorldFitBackend.dto.RegisterRequestDto;
 import com.C10G14.WorldFitBackend.entity.Role;
 import com.C10G14.WorldFitBackend.entity.User;
 import com.C10G14.WorldFitBackend.enumeration.ERole;
-import com.C10G14.WorldFitBackend.exception.InputNotValidException;
+import com.C10G14.WorldFitBackend.enumeration.ESex;
 import com.C10G14.WorldFitBackend.repository.RoleRepository;
-import com.C10G14.WorldFitBackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+
 
 @Component
 public class AuthDtoMapper {
@@ -29,11 +26,17 @@ public class AuthDtoMapper {
         Role userRole = roleRepository.findByName(ERole.ROLE_USER)
                 .orElseThrow(()-> new RuntimeException("Role USER not found"));
         roles.add(userRole);
+
+        ESex sex;
+        if (registerRequestDto.getSex().equals("male"))
+            sex = ESex.MALE;
+        else sex = ESex.FEMALE;
+
         return User.builder()
                 .email(registerRequestDto.getEmail())
                 .password(passwordEncoder.encode(registerRequestDto.getPassword()))
-                .profileImg(registerRequestDto.getProfilePicture())
-                .sex(registerRequestDto.getSex())
+                .profileImg(registerRequestDto.getProfileImg())
+                .sex(sex)
                 .age(registerRequestDto.getAge())
                 .height(registerRequestDto.getHeight())
                 .weight(registerRequestDto.getWeight())
