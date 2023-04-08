fdf86704d9cb142b2c1e1678c17925b20a727a47
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-04-08 18:26:31 -0300

    SP3-WF102: Refactorizados regex en RegisterRequestDto

--- Backend/WorldFitBackend/src/main/java/com/C10G14/WorldFitBackend/dto/RegisterRequestDto.java
+++ Backend/WorldFitBackend/src/main/java/com/C10G14/WorldFitBackend/dto/RegisterRequestDto.java
@@ -7,6 +7,7 @@ import org.hibernate.validator.constraints.URL;
 
 @Getter
 @Setter
+@Builder
 public class RegisterRequestDto {
     @NotBlank(message = "Email can't be empty")
     @Email (regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$",
@@ -14,20 +15,19 @@ public class RegisterRequestDto {
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private String email;
     @NotBlank(message = "Password can't be empty")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[.@#$%^&+=])(?=\\S+$).{8,20}$",
-            message = "Password must contain: at least 8 characters,1 digit, 1 upper case and 1 lower case letter and 1 special character")
+    @Pattern(regexp = "^([a-zA-Z0-9]+[ ,]*){8,20}$", message = "Password must contain at least 8 characters including letters, numbers, spaces and commas")
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
     private String password;
     @URL(message = "URL not valid")
-    private String profilePicture;
-    @Pattern(regexp = "\\d{2,3}",
-            message = "Weight must be in format 'kgs' example: '80'")
-    private String weight;
-    @Pattern(regexp = "^([0-2]*,)?[0-9]{2}+",
-            message = "Height must be in format: 'mt,cms' example: '1,80' ")
-    private String height;
-    @Pattern(regexp = "[MmFf]",
-            message = "Sex must be either m or f (no case sensitive)")
+    private String profileImg;
+    @DecimalMin(value = "0.00", message = "Weight must be greater than or equal to zero")
+    @DecimalMax(value = "999.99", message = "Weight must have two or fewer decimal places")
+    private Double weight;
+    @DecimalMin(value = "0.00", message = "Weight must be greater than or equal to zero")
+    @DecimalMax(value = "999.99", message = "Weight must have two or fewer decimal places")
+    private Double height;
+    @Pattern(regexp = "(?i)^(male|female)$",
+            message = "Sex must be either male or female (no case sensitive)")
     private String sex;
 
     @Max(value = 110,
