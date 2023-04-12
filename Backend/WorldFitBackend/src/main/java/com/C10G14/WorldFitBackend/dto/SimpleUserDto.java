c98b7314e505bab602817b9e9effc380f2317287
DiegoHaczek <diegohaczek1996@hotmail.com>
2023-04-12 17:07:01 -0300

    SP3-WF116: recibir id usuario en json

--- Backend/WorldFitBackend/src/main/java/com/C10G14/WorldFitBackend/dto/SimpleUserDto.java
+++ Backend/WorldFitBackend/src/main/java/com/C10G14/WorldFitBackend/dto/SimpleUserDto.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class SimpleUserDto {
+    private String id;
     private String email;
     private String name;
     private String clientSince;
