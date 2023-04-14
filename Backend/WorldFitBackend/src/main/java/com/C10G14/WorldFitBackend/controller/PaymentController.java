9ff5a3e0efa71d616585098f153d25e67d0de8fd
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2023-04-14 18:21:59 -0300

    SP3-WF123: Protegidos endpoints PaymentController

--- Backend/WorldFitBackend/src/main/java/com/C10G14/WorldFitBackend/controller/PaymentController.java
+++ Backend/WorldFitBackend/src/main/java/com/C10G14/WorldFitBackend/controller/PaymentController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -30,6 +31,7 @@ public class PaymentController {
             @ApiResponse(responseCode = "403", description = "Forbidden",
                     content = @Content)})
     @PostMapping("")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PaymentDto> addPayment(@RequestBody PaymentDto payment) {
         PaymentDto newPayment = paymentService.createPayment(payment);
         return new ResponseEntity<>(newPayment, HttpStatus.CREATED);
@@ -47,6 +49,7 @@ public class PaymentController {
             @ApiResponse(responseCode = "404", description = "Error: The payment does not exist",
                     content = @Content)})
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PaymentDto> getPaymentById(@PathVariable("id") Long id) {
         PaymentDto payment = paymentService.getPaymentById(id);
         return new ResponseEntity<>(payment, HttpStatus.OK);
@@ -64,6 +67,7 @@ public class PaymentController {
             @ApiResponse(responseCode = "404", description = "Error: The payment does not exist",
                     content = @Content)})
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PaymentDto> updatePayment(@PathVariable("id") Long id, @RequestBody PaymentDto payment) {
         PaymentDto updatedPayment = paymentService.updatePayment(id, payment);
         return new ResponseEntity<>(updatedPayment, HttpStatus.OK);
@@ -81,6 +85,7 @@ public class PaymentController {
             @ApiResponse(responseCode = "404", description = "Error: The payment does not exist",
                     content = @Content)})
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> deletePayment(@PathVariable("id") Long id) {
         paymentService.deletePayment(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
